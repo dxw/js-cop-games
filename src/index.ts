aa113dd2a71ecc0395e1ac70b36a29f869807e89
Ynda Jas <yndajas@gmail.com>
2023-09-14 17:04:24 +0100

    WIP throw it all away and start again

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { Elysia, ws } from "elysia";
-import { html } from "@elysiajs/html";
-
 const page = `<!DOCTYPE HTML>
 <html lang="en">
     <head>
@@ -15,47 +12,67 @@ const page = `<!DOCTYPE HTML>
         </form>
         <div id="message-list"></div>
     </body>
-    <script>
-    var loc = window.location, new_uri;
-    if (loc.protocol === "https:") {
-        new_uri = "wss:";
-    } else {
-        new_uri = "ws:";
-    }
-    new_uri += "//" + loc.host;
-    new_uri += loc.pathname + "hi";
 
-    const ws = new WebSocket(new_uri);
-    const form = document.querySelector("form");
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const message = e.target.message.value;
-        ws.send(message);
-    });
-    ws.open = (ws) => {
-      console.log(ws);
-    
-  };
-    ws.onmessage = (message) => {
-        console.log(message.data);
-        const newMessageP = document.createElement('p')
-        newMessageP.innerText = message.data
-        document.getElementById("message-list").appendChild(newMessageP)
-    };
-</script>
+    <script src="/socket.io/socket.io.js"></script>
+
+    <script>
+      const urlBuilder = () => {
+        var loc = window.location, new_uri;
+        if (loc.protocol === "https:") {
+            new_uri = "wss:";
+        } else {
+            new_uri = "ws:";
+        }
+        new_uri += "//" + loc.host;
+        new_uri += loc.pathname + "hi";
+        return new_uri;
+      }
+
+      const ws = new WebSocket(urlBuilder());
+      const form = document.querySelector("form");
+      form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          const message = e.target.message.value;
+          ws.send(message);
+      });
+      ws.open = (ws) => {
+        console.log(ws);
+      };
+      ws.onmessage = (message) => {
+        console.log('cat');
+          console.log(message.data);
+          const newMessageP = document.createElement('p')
+          newMessageP.innerText = message.data
+          document.getElementById("message-list").appendChild(newMessageP)
+      };
+    </script>
 </html>`;
 
-const app = new Elysia()
-  .use(html())
-  .get("/", ({ html }) => html(page))
-  .use(ws())
-  .ws("/hi", {
+Bun.serve({
+  fetch(req, server) {
+    const url = new URL(req.url);
+    if (url.protocol === "ws:") {
+      // upgrade the request to a WebSocket
+      if (server.upgrade(req)) {
+        return; // do not return a Response
+      }
+
+      return new Response("Upgrade failed :(", { status: 500 });
+    }
+
+    return new Response(page, {
+      headers: {
+        "Content-Type": "text/html",
+      },
+    });
+  },
+  websocket: {
     message(ws, message) {
-      console.log({ message, ws });
+      console.log({ message });
       ws.send(message);
     },
     open(ws) {
       ws.send("welcome");
     },
-  })
-  .listen(3000);
+  },
+});
