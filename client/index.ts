ec4ae7727c1f3acaab19913665d83fc53133b412
George Eaton <george@dxw.com>
2023-10-26 15:59:34 +0100

    WIP add colour checkboxes
    Very much WIP, we're trying to get the template to render properly but
    need to do a bit more reading of the docs...

--- client/index.ts
+++ client/index.ts
@@ -2,6 +2,7 @@ import { io } from "socket.io-client";
 import { NameFormElement } from "../server/@types/ui";
 import { Player, Question } from "../server/@types/models";
 import { getElementById } from "./utils/getElementById";
+import colours from "./data/colours.json";
 
 const addPlayer = async (name: string): Promise<void> => {
   socket.emit("players:post", { name });
@@ -30,6 +31,23 @@ const askAQuestion = (data: Question): void => {
   const numberHtml = getElementById("number");
   numberHtml.innerText = number.toString();
 };
+
+const createColourCheckboxElement = (colour: string): string {
+  return `<div>
+    <input type="checkbox" id="scales" name="scales" checked />
+    <label for="scales">Scales</label>
+  </div>`
+}
+
+const renderColourCheckboxes = (): void => {
+  const colourFieldset = getElementById("colour-fieldset");
+  const template = getElementById("checkbox-template") as HTMLTemplateElement;
+  
+  colours.forEach(colour => {
+    const clonedTemplate = template.content.cloneNode(true)
+    // what to do here!?
+  })
+}
 
 const derenderNameForm = (): void => {
   getElementById("name-form").remove();
