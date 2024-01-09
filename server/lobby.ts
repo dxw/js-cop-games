f1f1bb9a22c35a838539a1158ddd59d90baa86aa
Liz Daly <50752284+lookupdaily@users.noreply.github.com>
2024-01-09 16:42:27 +0000

    Add missing break in switch statement

--- server/lobby.ts
+++ server/lobby.ts
@@ -20,6 +20,7 @@ export default class Lobby {
       switch (state.value) {
         case 'MultiplePlayers':
           this.emitShowStartButton();
+          break;
         default:
           break;
       }
