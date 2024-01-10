3f7607bc1e4dda1849742d173cee100cc660d3e4
rich <rich.james@dxw.com>
2024-01-10 17:18:20 +0000

    Rename Game > Round

--- server/round.ts
+++ server/round.ts
@@ -1,9 +1,9 @@
 import { interpret } from "xstate";
 import { SocketServer } from "./socketServer";
-import { gameMachine, context } from "./machines/game";
+import { gameMachine, context } from "./machines/round";
 import questions from "./data/questions.json";
 
-export default class Game {
+export default class Round {
   server: SocketServer;
   machine;
 
@@ -12,7 +12,7 @@ export default class Game {
     this.machine = interpret(
       gameMachine.withContext({ ...context, questions }),
     ).start();
-    this.machine.start();
+
     this.machine.onTransition((state) => {
       console.info({ state: state.value, context: state.context });
     });
