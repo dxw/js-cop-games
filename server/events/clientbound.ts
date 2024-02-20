68052f9e9c42671423ec9115143465f4621430d3
Ynda Jas <yndajas@gmail.com>
2024-02-20 20:10:52 +0000

    Add event type wrapper
    We're a little confused about the `Payload` type here seeing as none of
    the payloads fulfil the full object typing (they only have one of the
    multiple keys in `Payload`, but it doesn't work as a union of all the
    possible payload objects, so we're going down this route for now
    
    Co-authored-by: Liz Daly <50752284+lookupdaily@users.noreply.github.com>
    Co-authored-by: Rich James <richpjames@users.noreply.github.com>

--- server/events/clientbound.ts
+++ server/events/clientbound.ts
@@ -2,18 +2,32 @@ import type { Player, Question } from "../@types/models";
 import type { Lobby } from "../lobby";
 
 const clientboundEvents = {
-	getPlayers: (lobby: Lobby): [string, { players: Player["name"][] }] => {
+	getPlayers: (lobby: Lobby): ClientboundSocketServerEvent<"players:get"> => {
 		return ["players:get", { players: lobby.playerNames() }];
 	},
-	getQuestion: (question: Question): [string, { question: Question }] => {
+	getQuestion: (
+		question: Question,
+	): ClientboundSocketServerEvent<"question:get"> => {
 		return ["question:get", { question }];
 	},
-	setPlayer: (player: Player): [string, { player: Player }] => {
+	setPlayer: (player: Player): ClientboundSocketServerEvent<"player:set"> => {
 		return ["player:set", { player }];
 	},
-	showStartButton: (): string => {
+	showStartButton: (): ClientboundSocketServerEvent<"round:startable"> => {
 		return "round:startable";
 	},
 };
+
+type Event = "round:startable" | "player:set" | "players:get" | "question:get";
+
+type Payloads = {
+	"question:get": { question: Question };
+	"players:get": { players: Player["name"][] };
+	"player:set": { player: Player };
+};
+
+type ClientboundSocketServerEvent<T = Event> = T extends keyof Payloads
+	? [T, Payloads[T]]
+	: T;
 
 export { clientboundEvents };
