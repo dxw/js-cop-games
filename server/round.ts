90d0b3d0e9b88988e56f29906b29c3b7008779b8
Ynda Jas <yndajas@gmail.com>
2024-02-20 17:53:26 +0000

    Emit selected question on game start
    Get the current question from the gameMachine context
    
    Co-authored-by: Liz Daly <50752284+lookupdaily@users.noreply.github.com>
    Co-authored-by: Rich James <richpjames@users.noreply.github.com>

--- server/round.ts
+++ server/round.ts
@@ -1,6 +1,7 @@
 import type { InterpreterFrom } from "xstate";
 import { interpret } from "xstate";
 
+import { Question } from "./@types/models";
 import questions from "./data/questions.json";
 import { context, gameMachine } from "./machines/round";
 import type { SocketServer } from "./socketServer";
@@ -17,6 +18,17 @@ class Round {
 
 		this.machine.onTransition((state) => {
 			console.info({ context: state.context, state: state.value });
+
+			switch (state.value) {
+				case "gameStart": {
+					this.server.onQuestionSet(
+						this.machine.getSnapshot().context.selectedQuestion as Question,
+					);
+					break;
+				}
+				default:
+					break;
+			}
 		});
 	}
 }
