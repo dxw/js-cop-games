1010b60cb19046060e889bae60fc529220f2d707
rich <rich.james@dxw.com>
2024-03-26 16:53:58 +0000

    Extend test to cover multiple player use
    This breaks down the tests into multiple methods and extends them by
    running two player sessions in parallel and checking that both render
    the relevant player name
    
    Co-authored-by: George <george@dxw.com>
    Co-authored-by: Rich James <richpjames@users.noreply.github.com>
    Co-authored-by: Liz Daly <50752284+lookupdaily@users.noreply.github.com>

--- e2e/app.test.ts
+++ e2e/app.test.ts
@@ -1,6 +1,44 @@
-import { expect, test } from "@playwright/test";
+import { type Browser, type Page, expect, test } from "@playwright/test";
+import type { Player } from "../server/@types/entities";
 
-test("test application", async ({ page }) => {
+const joinedPlayerNames: Player["name"][] = [];
+
+test("players can join the game", async ({ browser }) => {
+	const playersPages = await Promise.all([
+		createPage(browser),
+		createPage(browser),
+	]);
+
+	await Promise.all(
+		playersPages.map(async (playerPage, playerPageIndex) => {
+			await connect(playerPage);
+			const name = `Player ${playerPageIndex + 1}`;
+			await addName(playerPage, name);
+
+			//   Verify name is visible
+			await expect(
+				playerPage.getByText(`Name: ${name}`, { exact: true }),
+			).toBeVisible();
+		}),
+	);
+
+	//   Verify full player list is visible on all players' browsers
+	for (const playerPage of playersPages) {
+		for (const joinedPlayerName of joinedPlayerNames) {
+			await expect(
+				playerPage.getByRole("listitem").getByText(joinedPlayerName),
+			).toBeVisible();
+		}
+	}
+});
+
+const createPage = async (browser: Browser) => {
+	const context = await browser.newContext();
+	const page = await context.newPage();
+	return page;
+};
+
+const connect = async (page: Page) => {
 	await page.goto("/");
 
 	//   Verify page title
@@ -8,16 +46,15 @@ test("test application", async ({ page }) => {
 
 	//   Verify colour is visible
 	await page.getByText("Connected 🟢");
+};
 
+const addName = async (page: Page, name: Player["name"]) => {
 	//   Enter name
-	const displayNameInput = await page.getByLabel("Display name");
-	displayNameInput.click();
-	displayNameInput.fill("Test name");
+	const displayNameInput = page.getByLabel("Display name");
+	await displayNameInput.fill(name);
 
 	//   Click join
-	await page.getByRole("button", { name: "Join game" }).click();
-
-	//   Verify name is visible
-	await page.getByText("Name: Test name");
-	await page.getByText("Test name", { exact: true });
-});
+	const joinButton = page.getByRole("button", { name: "Join game" });
+	await joinButton.click();
+	joinedPlayerNames.push(name);
+};
