0892c1f7bea97b6e25e1bd8d3c97f0b6b3d915dc
Ynda Jas <yndajas@gmail.com>
2024-08-27 18:48:50 +0100

    Sort events interfaces

--- server/@types/events.d.ts
+++ server/@types/events.d.ts
@@ -1,20 +1,20 @@
 import type { Colour, Player, Question } from "./entities";
 
 export interface ClientboundSocketServerEvents {
+	"answers:post": (playerId: string, colours: string[]) => void;
 	"lobby:unjoinable": () => void;
-	"round:startable": () => void;
-	"round:start": () => void;
-	"round:reset": () => void;
 	"player:set": (player: Player) => void;
 	"players:get": (playerNames: Player["name"][]) => void;
 	"question:get": (question: Question) => void;
-	"answers:post": (playerId: string, colours: string[]) => void;
+	"round:reset": () => void;
+	"round:start": () => void;
+	"round:startable": () => void;
 }
 
 export interface ServerboundSocketServerEvents {
 	"answers:post": (colours: Colour[]) => void;
 	disconnect: () => void;
 	"players:post": (name: Player["name"]) => void;
-	"round:start": () => void;
 	"round:reset": () => void;
+	"round:start": () => void;
 }
