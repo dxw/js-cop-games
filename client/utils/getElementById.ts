07a3dbb7d6395aedade731babc74869c91620aef
rich <rich.james@dxw.com>
2024-08-30 20:27:44 +0100

    Don't throw element if not found

--- client/utils/getElementById.ts
+++ client/utils/getElementById.ts
@@ -1,9 +1,9 @@
 export const getElementById = <T extends HTMLElement>(id: T["id"]): T => {
 	const element = document.getElementById(id) as T;
 
-	if (!element) {
-		throw new Error(`No element found with ID: ${id}`);
-	}
+	// if (!element) {
+	// 	throw new Error(`No element found with ID: ${id}`);
+	// }
 
 	return element;
 };
